b0c0cf3ded77e5bf52c9b17d342ba081215973f9
Energised <woolsey64@gmail.com>
2019-01-13 15:14:26 +0000

    more non-planar code added

--- non_planar.py
+++ non_planar.py
@@ -9,6 +9,10 @@ import fileinput
 import re
 import sys
 
+import math as m
+
+PI = m.pi
+
 # all distance values are measured in (mm)
 parameters = {"wave_amplitude": 2.0,
               "wave_length": 20.0,
@@ -33,14 +37,17 @@ lastGcodeZ = gcodeZ
 lastGcodeE = gcodeE
 lastGcodeF = gcodeF
 
+input_buffer = []
+output_buffer = []
+
 # MATHS
 # Various functions required for calculations
 
 def dist3(x1,y1,z1,x2,y2,z2):
-    return sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+    return m.sqrt(m.pow(x2-x1,2.0) + m.pow(y2-y1,2.0) + m.pow(z2-z1,2.0))
 
 def dist2(x1,y1,x2,y2):
-    return sqrt((x2-x1)^2 + (y2-y1)^2)
+    return m.sqrt(m.pow(x2-x1,2.0) + m.pow(y2-y1,2.0))
 
 def dist1(s1,s2):
     return abs(s2-s1)
@@ -49,8 +56,81 @@ def digitise(num, digits):
     factor = 10^digits
     return float(round(num*factor)/factor)
 
-input_buffer = []
-output_buffer = []
+def calculate_ramps(z):
+    rampA = max(min(((z-parameters["wave_in"])/(parameters["wave_ramp"])),1.0),0.0)
+    rampB = 1.0 - (max(min(((z-parameters["wave_out"]+parameters["wave_ramp"])/parameters["wave_ramp"]),1.0),0.0))
+    return rampA * rampB
+
+def calculate_z_displacement(x,y,z):
+    ramps = calculate_ramps(z)
+    z_offset = 0
+    amplitude = parameters["wave_amplitude"]
+    bed_center_x = parameters["bed_center_x"]
+    bed_center_y = parameters["bed_center_y"]
+    wave_length = parameters["wave_length"]
+    wave_length_2 = parameters["wave_length_2"]
+    if(parameters["wave_function"] == "wave"):
+        z_offset = 0.0 - amplitude/2.0 + (amplitude/(4.0*m.sin(((x-bed_center_x)*2.0*PI)/wave_length))) + (amplitude/(4.0*m.sin(((y-bed_center_y)*2.0*PI)/wave_length)))
+    elif(parameters["wave_function"] == "wing"):
+        y_displacement = y - bed_center_y - (wave_length_2/4)
+        z_offset = -amplitude/2.0 + amplitude * (m.sin(m.pow((((x-bed_center_x)*m.sqrt(PI))/amplitude - (m.sqrt(PI)/2.0)),2.0))) * (1.0 + 0.5*m.cos((2.0*PI*y_displacement)/wave_length_2))
+    elif(parameters["wave_function"]):
+        return eval(parameters["wave_function"])
+    else:
+        print("> no wave function found")
+    z_offset *= ramps
+    return z_offset
+
+def calculate_extrusion_multiplier(x,y,z):
+    ramps = calculate_ramps(z)
+    layer_height = 1.0 # must be 1 because divide by 0 error
+    # this will be an issue
+    if(parameters["wave_function"] == "wave"):
+        layer_height = y - parameters["bed_center_y"] - (parameters["wave_length"]/4.0)
+    elif(parameters["wave_function"] == "wing"):
+        layer_height = y - parameters["bed_center_y"] - (parameters["wave_length_2"]/4.0)
+    else:
+        print("cannot calculate y_displacement without extra parameters for user defined wave_function")
+    this = calculate_z_displacement(x,y,z)
+    last = calculate_z_displacement(x,y,(z-layer_height))
+    multiplier = 1.0 + ((this-last)/layer_height)
+    return multiplier
+
+def displace_move(line,x,y,z,e,f): # optional verbose argument for ...
+    if(gcodeZ >= parameters["wave_in"] and gcodeZ <= parameters["wave_out"]):
+        op_x = x or lastGcodeX
+        op_y = y or lastGcodeY
+        op_z = z or lastGcodeZ
+        op_e = e or lastGcodeE
+        op_f = f or lastGcodeF
+
+        distance = dist2(lastGcodeX, lastGcodeY, op_x, op_y)
+        segments = max(m.ceil(distance/parameters["wave_max_segment_length"]),1)
+
+        gcode = "; displaced move start (" + segments + "segments)\n"
+
+        for(k=0, k<segments; k++):
+            segmentX = (lastGcodeX + (k+1)) * ((op_x - lastGcodeX)/segments)
+            segmentY = (lastGcodeY + (k+1)) * ((op_y - lastGcodeY)/segments)
+            segmentZ = (lastGcodeZ + (k+1)) * ((op_z - lastGcodeZ)/segments)
+            segmentE = gcodeE/segments # only for relative extrusion
+
+            segmentE *= calculate_extrusion_multiplier(segmentX, segmentY, segmentZ)
+            segmentZ += calculate_z_displacement(segmentX, segmentY, segmentZ)
+
+            wave_digits = parameters["wave_digits"]
+
+            gcode += "G1"
+            gcode += " X" + str(digitise(segmentX, wave_digits))
+            gcode += " Y" + str(digitise(segmentY, wave_digits))
+            gcode += " Z" + str(digitise(segmentZ, wave_digits))
+            gcode += " E" + str(digitise(segmentE, wave_digits))
+            gcode += " F" + str(op_f)
+            gcode += " ; segment " + str(k) + "\n"
+        gcode += " ; displaced move end \n"
+        return gcode
+    else:
+        return line
 
 # REGULAR EXPRESSIONS
 # For use in filter_print_gcode
@@ -107,21 +187,20 @@ def process_comment(line, comment):
 
 def process_layer_change(line, z):
     # do stuff
-    return line
+    return displace_move(line,x=0.0,y=0.0,z,e=0.0,f=0.0)
 
 def process_retraction_move(line, e, f):
     # do stuff
     return line
 
 def process_printing_move(line, x, y, z, e, f):
-    print("process_printing_move: " + line + " - " + str(x) + ',' + str(y) +
-          ',' + str(z) + ',' + str(e) + ',' + str(f))
+    #print("process_printing_move: " + line + " - " + str(x) + ',' + str(y) + ',' + str(z) + ',' + str(e) + ',' + str(f))
     # do stuff
-    return line
+    return displace_move(line,x,y,z,e,f)
 
 def process_travel_move(line, x, y, z, f):
     # do stuff
-    return line
+    return displace_move(line,x,y,z,e=0.0,f)
 
 def process_absolute_extrusion(line):
     # do stuff
@@ -240,13 +319,30 @@ def filter_print_gcode(line):
         if other_re.match(line):
             return process_other(line)
 
+# testing area
+
+def test_set_1():
+    print("> dist3() = " + str(dist3(24.3, 56.4, 2.13, 11, 64.5, 34.1)))
+    print("> dist2() = " + str(dist2(56.32, 24, 76.2, 12)))
+    print("> dist1() = " + str(dist1(23.4, 19.3)))
+    print("> digitise() = " + str(digitise(45.32, 4)))
+    print("> calculate_ramps() = " + str(calculate_ramps(2)))
+    print("> calculate_z_displacement() = " + str(calculate_z_displacement(4,8,3)))
+    print("> calculate_extrusion_multiplier() = " + str(calculate_extrusion_multiplier(16,24,9)))
+
 
 # main loop
-with fileinput.input() as f:
-    for line in f: # iterates line by line for file in sys.argv[1]
-        #print(line,end='')
-        line = line.strip('\n') # need to check if this is ok
-        filter_parameters(line)
-        input_buffer.append(line)
-    process_buffer()
-    init()
+
+def main():
+    with fileinput.input() as f:
+        for line in f: # iterates line by line for file in sys.argv[1]
+            #print(line,end='')
+            line = line.strip('\n') # need to check if this is ok
+            filter_parameters(line)
+            input_buffer.append(line)
+        process_buffer()
+        init()
+
+if __name__ == "__main__":
+    #main()
+    test_set_1()
